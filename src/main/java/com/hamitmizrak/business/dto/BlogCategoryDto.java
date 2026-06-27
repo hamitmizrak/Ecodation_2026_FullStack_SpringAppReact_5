31c340398504a4b42ca22e503ceddeba34e88b0d
Hamit Mızrak <hamitmizrak@gmail.com>
2026-06-27 10:37:05 +0300

    BlogCategoryDto BlogDto

--- src/main/java/com/hamitmizrak/business/dto/BlogCategoryDto.java
+++ src/main/java/com/hamitmizrak/business/dto/BlogCategoryDto.java
@@ -6,24 +6,32 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
 
+// Lombok
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+
+// BlogCategory(1) - BlogDto(N)
 public class BlogCategoryDto extends AuditingAwareBaseDto implements Serializable {
 
     // Serial
+    @Serial
     private static final long serialVersionUID = 1L;
 
     // Field
+
+    // ID
     private String categoryId;
 
+    // categoryName
     @NotEmpty(message = "{blog.category.least.validation.constraints.NotNull.message}")
     @Size(min = 3, message = "{blog.category.unique.validation.constraints.NotNull.message}")
     private String categoryName;
-    private Date systemCreatedDate;
+
 }
---
+++ src/main/java/com/hamitmizrak/business/dto/BlogDto.java
@@ -0,0 +1,48 @@
+package com.hamitmizrak.business.dto;
+
+
+import com.hamitmizrak.audit.AuditingAwareBaseDto;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+// Lombok
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+//  BlogDto(N) -BlogCategory(1)
+public class BlogDto  extends AuditingAwareBaseDto implements Serializable {
+
+    // Serial
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    // Field
+    private Long blogId;
+
+    // Header
+    @NotEmpty(message = "{blog.header.least.validation.constraints.NotNull.message}")
+    @Size(min = 3, message = "{blog.header.least.validation.constraints.NotNull.message}")
+    private String header;
+
+    // Title
+    @NotEmpty(message = "{blog.title.least.validation.constraints.NotNull.message}")
+    private String title;
+
+    // Content
+    @NotEmpty(message = "{blog.content.least.validation.constraints.NotNull.message}")
+    private String content;
+
+    // Picture
+    @Builder.Default
+    private String image="resim.png";
+
+    // Relation
+    private BlogCategoryDto blogCategoryDto;
+}
